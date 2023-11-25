e1f27d83a04ee1258ddee1dba33ebcc69d399205
chris <chunghao777@gmail.com>
2023-11-25 21:00:09 +0800

    update to package.json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 /*
 var mysql = require('mysql');
